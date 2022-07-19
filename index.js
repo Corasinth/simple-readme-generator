9d90a0dbf54b85ed6bdf8aed6feb59a33f81ee97
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-18 22:40:13 -0500

    fill out framework of starter functions

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const markdown = require("generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    {
+    type: "input",
+    message: "What's your name?",
+    name: "name"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function writeToFile(fileName, data) {
+    fs.writeFile('readme.md', data, (err) => {
+        err ? console.error(err) : console.log('readme.md has been succesfully generated')
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    let response = await inquirer.prompt(questions);
+    writeToFile(TBD, TBD)
+};
 
 // Function call to initialize app
 init();
